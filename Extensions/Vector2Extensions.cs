4a533c75c9ad481de7da300014df627d0d76a003
JvizcainoM <45899392+JvizcainoM@users.noreply.github.com>
2024-01-12 21:14:24 -0600

    vector 2 extentions

---
+++ Extensions/Vector2Extensions.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public static class Vector2Extensions {
+    /// <summary>
+    /// Sets any x y values of a Vector2
+    /// </summary>
+    public static Vector2 With(this Vector2 vector, float? x = null, float? y = null) {
+        return new Vector2(x ?? vector.x, y ?? vector.y);
+    }
+
+    /// <summary>
+    /// Adds to any x y values of a Vector2
+    /// </summary>
+    public static Vector2 Add(this Vector2 vector, float x = 0, float y = 0) {
+        return new Vector2(vector.x + x, vector.y + y);
+    }
+
+    /// <summary>
+    /// Returns a Boolean indicating whether the current Vector2 is in a given range from another Vector2
+    /// </summary>
+    /// <param name="current">The current Vector2 position</param>
+    /// <param name="target">The Vector2 position to compare against</param>
+    /// <param name="range">The range value to compare against</param>
+    /// <returns>True if the current Vector2 is in the given range from the target Vector2, false otherwise</returns>
+    public static bool InRangeOf(this Vector2 current, Vector2 target, float range) {
+        return (current - target).sqrMagnitude <= range * range;
+    }
+}
